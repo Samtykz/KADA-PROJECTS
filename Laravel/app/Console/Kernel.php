89000ab590eea1b6626097f652884b2c8db6288e
Samtykz <samtdiaz31@gmail.com>
2025-06-15 05:19:37 +0000

    require_once en kernel.php

--- Laravel/app/Console/Kernel.php
+++ Laravel/app/Console/Kernel.php
@@ -12,7 +12,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
     }
 
     /**
@@ -21,6 +20,6 @@ class Kernel extends ConsoleKernel
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-        require base_path('routes/console.php');
+        require_once base_path('routes/console.php');
     }
 }
