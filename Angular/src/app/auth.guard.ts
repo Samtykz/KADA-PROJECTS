2694113b7e572eed5b532b4bffa04344aba97c75
Samtykz <samtdiaz31@gmail.com>
2025-06-15 02:33:23 +0000

    Se combinaron las importaciones de CanActivateFn y Router

--- Angular/src/app/auth.guard.ts
+++ Angular/src/app/auth.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
