435d7e3e9b1dffee0c62a14d57aa9fe1cc59cea5
Samtykz <samtdiaz31@gmail.com>
2025-06-15 02:13:47 +0000

    eliminacion de imports de componentes (inicio, registro y login) de app.component.ts

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -4,9 +4,6 @@ import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from "./componentes/cabecera/cabecera.component";
 import { NavegacionComponent } from "./componentes/navegacion/navegacion.component";
 import { PieComponent } from "./componentes/pie/pie.component";
-import { InicioComponent } from './componentes/inicio/inicio.component';
-import { RegistroClienteComponent } from './componentes/registro-cliente/registro-cliente.component';
-import { InicioSesionComponent } from './componentes/inicio-sesion/inicio-sesion.component';
 
 @Component({
   selector: 'app-root',
