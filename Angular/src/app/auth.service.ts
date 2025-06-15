b072325cc5a8c159bafbe35ce34bd9f01e5f0c78
Samtykz <samtdiaz31@gmail.com>
2025-06-15 02:30:53 +0000

    actualización de auth.service.ts

--- Angular/src/app/auth.service.ts
+++ Angular/src/app/auth.service.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private readonly router: Router) { }
 
-  constructor(private router: Router) { }
-
-  isAuthenticated() : boolean {
+  isAuthenticated(): boolean {
     if (typeof window !== 'undefined' && window.localStorage) {
-      return !!localStorage.getItem('token')
+      return !!localStorage.getItem('token');
     }
     return false;
   }
 
-  
   logout() {
     if (typeof window !== 'undefined' && window.localStorage) {
       localStorage.removeItem('token');
-      console.log('Token elimidado')
+      console.log('Token eliminado');
     }
     this.router.navigate(['inicio-sesion']);
   }
-
-
-
 }
