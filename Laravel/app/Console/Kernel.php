ea3114db5d3ee8c521101c795f99e138d8c1ce74
Samtykz <samtdiaz31@gmail.com>
2025-06-15 05:23:33 +0000

    kernel actualizado

--- Laravel/app/Console/Kernel.php
+++ Laravel/app/Console/Kernel.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Console;
-
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,7 +10,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
     }
-
     /**
      * Register the commands for the application.
      */
