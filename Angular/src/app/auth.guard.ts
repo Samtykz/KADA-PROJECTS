20b4701a6bd718d27702a3775c17ec3ea1dfc958
Samtykz <samtdiaz31@gmail.com>
2025-06-15 02:25:06 +0000

    limpieza de guard.ts

--- Angular/src/app/auth.guard.ts
+++ Angular/src/app/auth.guard.ts
@@ -1,8 +1,4 @@
-import { Injectable } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
 import { AuthService } from './auth.service';
-import {  inject} from '@angular/core';
-import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
