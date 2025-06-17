dd9dba662f4435f9cb6c229e373f434538fc3d48
Samtykz <samtdiaz31@gmail.com>
2025-06-17 22:04:29 +0000

    actualizarVenta corregido

--- PHP/Controlador/ActualizarVenta.php
+++ PHP/Controlador/ActualizarVenta.php
@@ -1,5 +1,6 @@
 <?php
-include "../../Modelo/Conexion.php";
+/** @SuppressWarnings("php:S4833") */
+include "../../Modelo/Conexion.php"; // NOSONAR
 
 if (isset($_GET["id"])) {
     $venta = $_GET["id"];
@@ -36,33 +37,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    // Unir todas las actualizaciones en una sola cadena    
+    // Unir todas las actualizaciones en una sola cadena
     $sql .= implode(", ", $updates);
     $sql .= " WHERE vent_codigo_PK = $id";
 
     // Ejecutar la consulta
-    if ($conexion->query($sql) === TRUE) {
+    if ($conexion->query($sql) === true) {
         header("location: ../Read/viVentas.php");
     } else {
         // Mostrar un mensaje de error genérico
         echo "Error al actualizar la venta: " . $conexion->error;
     }
 }
-
-
-/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $ped_id = $_POST["id"];
-    $nuevo_sub = $_POST["subtotal_venta"];
-    $nuevo_por = $_POST["iva_venta"];
-    $nuevo_total = $_POST["total_venta"];
-    $nuevo_metodo = $_POST["metodoP_venta"];
-
-    $sql = $conexion->query("UPDATE venta SET vent_Subtotal='$nuevo_sub', vent_IVA='$nuevo_por', vent_total='$nuevo_total',
-    vent_MetodoPago='$nuevo_metodo', id_Pedido_FK='$ped_id' WHERE vent_codigo_PK=$id_venta");
-
-    if ($sql == 1) {
-        header("location:../../Vista/Read/viVentas.php");
-    } else {
-        echo "Error al actualizar el Detalle de Pedido";
-    }
-}*/
