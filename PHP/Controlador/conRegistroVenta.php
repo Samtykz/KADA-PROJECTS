ba9b2508244066b100e074fdc057f5dc676420b6
Samtykz <samtdiaz31@gmail.com>
2025-06-17 23:15:28 +0000

    conRegistroVenta en proceso de correción

--- PHP/Controlador/conRegistroVenta.php
+++ PHP/Controlador/conRegistroVenta.php
@@ -1,6 +1,6 @@
 <?php
-// Incluir conexión a base de datos
-require_once '../../Modelo/Conexion.php';
+/** @SuppressWarnings("php:S4833") */
+require_once '../../Modelo/Conexion.php'; // NOSONAR
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
@@ -42,4 +42,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'  && isset($_POST['btnRegistrarVenta']))
         echo 'Error al registrar.';
     }
 }
-?>
