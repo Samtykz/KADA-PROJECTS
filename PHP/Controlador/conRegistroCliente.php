c1c2cb04b18f6af5bfca58431d35fa0c4437f96a
Samtykz <samtdiaz31@gmail.com>
2025-06-17 23:02:42 +0000

    registro cliente en proceso (falta seguridad)

--- PHP/Controlador/conRegistroCliente.php
+++ PHP/Controlador/conRegistroCliente.php
@@ -1,5 +1,6 @@
 <?php
-require_once '../../Modelo/Conexion.php';
+/** @SuppressWarnings("php:S4833") */
+require_once '../../Modelo/Conexion.php'; // NOSONAR
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
@@ -42,4 +43,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['registrarClie'])) {
         echo 'Error al registrar.';
     }
 }
-?>
