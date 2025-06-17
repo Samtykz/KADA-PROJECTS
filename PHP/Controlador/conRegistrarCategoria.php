48e330b8b7c95090b5d63a254c67cf4a002549da
Samtykz <samtdiaz31@gmail.com>
2025-06-17 22:40:35 +0000

    if corregido  en registrarCategoria

--- PHP/Controlador/conRegistrarCategoria.php
+++ PHP/Controlador/conRegistrarCategoria.php
@@ -2,20 +2,16 @@
 /** @SuppressWarnings("php:S4833") */
 include_once "../../Modelo/Conexion.php"; // NOSONAR
 
-if (!empty($_POST["btnagregar"])) {
-   if (!empty($_POST["nombreCategoria"])) {
-        // Recuperar datos del formulario
-       $namecategoria = $_POST["nombreCategoria"];
-
-       // Insertar datos en la base de datos
-       $sql=$conexion->query("INSERT INTO productocategoria(nombreCategoria)
-       values('$namecategoria')");
-         if ($sql==1) {
+if (!empty($_POST["btnagregar"]) && !empty($_POST["nombreCategoria"])) {
+    // Recuperar datos del formulario
+    $namecategoria = $_POST["nombreCategoria"];
+    // Insertar datos en la base de datos
+    $sql = $conexion->query("INSERT INTO productocategoria(nombreCategoria) VALUES('$namecategoria')");
+    if ($sql == 1) {
         echo "<div class='alert alert-success'>La categoría se ha registrado correctamente</div>";
         header("location:../../Vista/Read/categorias.php");
-       } /*else{
+    }
+    /* else {
         echo "<div class='alert alert-danger'>Error al registrar la categoría</div>";
-       }*/
-
-   }
+    } */
 }
