9ba3460bbf59f47d93217392edcaf6451fae12f9
Samtykz <samtdiaz31@gmail.com>
2025-06-17 21:27:19 +0000

    actualizaradmin corregido completamente

--- PHP/Controlador/ActualizarAdmin.php
+++ PHP/Controlador/ActualizarAdmin.php
@@ -1,7 +1,6 @@
 <?php
-include_once "../../Modelo/Conexion.php";
-
-
+/** @SuppressWarnings("php:S4833") */
+include_once "../../Modelo/Conexion.php"; // NOSONAR
 // Obtener el ID del administrador desde la URL
 if (isset($_GET["id"])) {
     $idAdministrador = $_GET["id"];
@@ -9,7 +8,6 @@ if (isset($_GET["id"])) {
     $resultado = $conexion->query($consulta);
     $Admin = $resultado->fetch_assoc();
 }
-
 // Procesar el formulario cuando se envía
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST["id"];
@@ -18,12 +16,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telefono = $_POST["telefonoNuevo"];
     $direccion = $_POST["direccionNueva"];
     $correo = $_POST["correoNuevo"];
-
-
     // Construir la consulta SQL dinámicamente
     $sql = "UPDATE Administrador SET ";
     $updates = array();
-
     if (!empty($nombre)) {
         $updates[] = "admi_nombre='$nombre'";
     }
@@ -39,24 +34,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($correo)) {
         $updates[] = "admi_correo='$correo'";
     }
-
-
     // Verificar si hay algo que actualizar
     if (empty($updates)) {
         // Si no hay nada que actualizar, redirigir con un mensaje de error
         header("location: ../Read/viAdministradores.php?error=1");
         exit();
     }
-
     // Unir todas las actualizaciones en una sola cadena
     $sql .= implode(", ", $updates);
     $sql .= " WHERE admi_Codigo_PK = $id";
-
     // Ejecutar la consulta
-    if ($conexion->query($sql) === TRUE) {
+    if ($conexion->query($sql) === true) {
         header("location: ../Read/viAdministradores.php");
     } else {
         // Mostrar un mensaje de error genérico
         echo "Error al actualizar el administrador: " . $conexion->error;
     }
 }
+
