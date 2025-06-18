f58a47c2e790e73c58e53e6675e012d5d5a6c75c
Samtykz <samtdiaz31@gmail.com>
2025-06-18 05:07:56 +0000

    ActualizarPed terminado

--- PHP/Vista/Update/ActualizarPed.php
+++ PHP/Vista/Update/ActualizarPed.php
@@ -16,9 +16,10 @@
 <body style="position: relative; padding-bottom: 10rem; min-height: 100vh;">
   <br><br>
   <?php
-  /** @SuppressWarnings("php:S4833") */
+    /** @SuppressWarnings("php:S4833") */
     include_once "../../Modelo/Conexion.php"; // NOSONAR
-    $id = $_GET["id"];
+    // Only allow integer IDs from GET variable for security
+    $id = isset($_GET["id"]) && ctype_digit($_GET["id"]) ? (int)$_GET["id"] : 0;
     $sql = $conexion->query("SELECT * from pedido where id_Pedido_PK = $id");
   ?>
   <section class="h-100">
@@ -30,7 +31,7 @@
               <div class="card-body p-md-8 text-black">
                 <form id="formActualizarPedido" method="POST">
                   <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Modificar Pedido</p>
-                  <input type="hidden" name="id" value="<?= $_GET["id"] ?>">
+                  <input type="hidden" name="id" value="<?php echo htmlspecialchars($id, ENT_QUOTES, 'UTF-8'); ?>">
                   <?php
                   /** @SuppressWarnings("php:S4833") */
                   include_once "../../Controlador/ActualizarPedido.php"; // NOSONAR
@@ -38,14 +39,14 @@
                   <div class="row">
                     <div class="col-md-6 mb-4">
                       <div data-mdb-input-init class="form-outline">
-                        <input type="number" class="form-control" name="docClie" value="<?php echo $pedido["clie_Documento_FK"]; ?>" readonly>
-                        <label class="form-label">Documento del Cliente</label>
+                        <input type="number" id="docClie" class="form-control" name="docClie" value="<?php echo $pedido["clie_Documento_FK"]; ?>" readonly>
+                        <label for="docClie" class="form-label">Documento del Cliente</label>
                       </div>
                     </div>
                     <div class="col-md-6 mb-4">
                       <div data-mdb-input-init class="form-outline">
                         <input type="number" id="nuDoc" class="form-control" name="docNueClie">
-                        <label class="form-text">Ingresa el nuevo documento del Cliente</label>
+                        <label for="nuDoc" class="form-text">Ingresa el nuevo documento del Cliente</label>
                       </div>
                     </div>
                   </div>
@@ -115,3 +116,4 @@
   </script>
 </body>
 </html>
+
