08b99bce2ff96f141ce7ea63507fd29413b6a6b7
Samtykz <samtdiaz31@gmail.com>
2025-06-18 06:50:52 +0000

    servicios.php actualizado

--- PHP/Vista/servicios.php
+++ PHP/Vista/servicios.php
@@ -10,132 +10,117 @@
   ?>
   <meta name="description" content="" />
   <meta name="keywords" content="bootstrap, bootstrap5" />
-
-		<!-- Bootstrap CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-		<link href="css/tiny-slider.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
-		<title>KADA - SERVICIOS </title>
-	</head>
-
-	<body style="position: relative; padding-bottom: 3rem; min-height: 100vh;">
-
-		<!-- Start Header/Navigation -->
-	
-		<!-- End Header/Navigation -->
-
-		<!-- Start Hero Section -->
-			<div class="hero">
-				<div class="container">
-					<div class="row justify-content-between">
-						<div class="col-lg-5">
-							<div class="intro-excerpt">
-								<h1>Servicios</h1>
-								<p class="mb-4">Aquí puedes informarte acerca de las ventajas que ofrecemos. </p>
-							</div>
-						</div>
-						<div class="col-lg-7">
-							<div class="hero-img-wrap">
-								<img src="images/LKADA.png" class="img-fluid">
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		<!-- End Hero Section -->
-
-		
-
-		<!-- Start Why Choose Us Section -->
-		<div class="why-choose-section">
-			<div class="container">
-				
-				
-				<div class="row my-5">
-					<div class="col-6 col-md-6 col-lg-3 mb-4">
-						<div class="feature">
-							<div class="icon">
-								<img src="images/truck.svg" alt="Image" class="imf-fluid">
-							</div><br>
-							<h3>Entrega Rápida &amp; Gratuita</h3>
-							<p>Nos complace ofrecerte nuestro nuevo servicio de Entrega Rápida y Gratuita. Realiza tu pedido hoy y recíbelo en la puerta de tu casa en tiempo récord, sin ningún costo adicional.</p>
-						</div>
-					</div>
-
-					<div class="col-6 col-md-6 col-lg-3 mb-4">
-						<div class="feature">
-							<div class="icon">
-								<img src="images/bag.svg" alt="Image" class="imf-fluid">
-							</div><br>
-							<h3>Fácil de Comprar</h3>
-							<p>Realizar tus compras nunca ha sido tan sencillo. Navega por nuestra tienda online, selecciona tus productos favoritos y completa tu compra.</p>
-						</div>
-					</div>
-
-					<div class="col-6 col-md-6 col-lg-3 mb-4">
-						<div class="feature">
-							<div class="icon">
-								<img src="images/support.svg" alt="Image" class="imf-fluid">
-							</div><br>
-							<h3>Soporte 24/7</h3>
-							<p>Con nuestro servicio de Soporte 24/7, puedes contar con asistencia técnica y atención al cliente las 24 horas del día, los 7 días de la semana.</p>
-						</div>
-					</div>
-
-					<div class="col-6 col-md-6 col-lg-3 mb-4">
-						<div class="feature">
-							<div class="icon">
-								<img src="images/return.svg" alt="Image" class="imf-fluid">
-							</div><br>
-							<h3>Periodo de devolución</h3>
-							<p>Ofrecemos un período de devolución de X días. Si por alguna razón no estás contento con tu producto, puedes devolverlo sin problemas.</p>
-						</div>
-					</div>
-				</div>
-			</div><br><br><br><br><br><br><br><br><br><br><br><br>
-		<!-- End Why Choose Us Section -->
-
-		<!-- Start Footer Section -->
-		<footer class="footer-section" style="padding: 1em 0; justify-content: center; align-items: center; position: absolute; bottom: 0; width: 100%;">
-			<div class="container relative">
-				<div class="row g-5 mb-5">
-					<div class="col-lg-4 ">
-						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">KADA</a></div>
-						<p class="mb-4">Bienvenido a Sistema de información KADA, un sitio web en el cuál podras encontrar buzos únicos y a la moda. En cada prenda aseguramos que obtengas la máxima comodidad y estilo. Nos enorgullece ofrecer una amplia variedad de buzos que se adaptan a todos los gustos y ocasiones. ¡GRACIAS POR ELEGIRNOS!	</p>
-					</div>
-					<div class="col-lg-8">
-						<div class="row links-wrap">
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="services.html">Servicios</a></li>
-									<li><a href="contact.html">Contáctanos</a></li>
-								</ul>
-							</div>
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="shop.html#halfzip">Half Zip Sweaters</a></li>
-									<li><a href="shop.html#oversize">Oversize Hoodies</a></li>
-									<li><a href="shop.html#hoodies">Hoodies</a></li>
-									<li><a href="shop.html#crewnecks">Crewnecks</a></li>
-								</ul>
-							</div>
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="LoginAdmi.html">¿Eres Administrador?</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!-- End Footer Section -->
-
-
-		<script src="js/bootstrap.bundle.min.js"></script>
-		<script src="js/tiny-slider.js"></script>
-		<script src="js/custom.js"></script>
-	</body>
-
+  <!-- Bootstrap CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+  <link href="css/tiny-slider.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <title>KADA - SERVICIOS </title>
+</head>
+<body style="position: relative; padding-bottom: 3rem; min-height: 100vh;">
+  <!-- Start Header/Navigation -->
+  <!-- End Header/Navigation -->
+  <!-- Start Hero Section -->
+  <div class="hero">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-5">
+          <div class="intro-excerpt">
+            <h1>Servicios</h1>
+            <p class="mb-4">Aquí puedes informarte acerca de las ventajas que ofrecemos. </p>
+          </div>
+        </div>
+        <div class="col-lg-7">
+          <div class="hero-img-wrap">
+            <img src="images/LKADA.png" class="img-fluid" alt="Logo KADA">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Hero Section -->
+  <!-- Start Why Choose Us Section -->
+  <div class="why-choose-section">
+    <div class="container">
+      <div class="row my-5">
+        <div class="col-6 col-md-6 col-lg-3 mb-4">
+          <div class="feature">
+            <div class="icon">
+              <img src="images/truck.svg" alt="Entrega Rápida y Gratuita" class="imf-fluid">
+            </div><br>
+            <h3>Entrega Rápida &amp; Gratuita</h3>
+            <p>Nos complace ofrecerte nuestro nuevo servicio de Entrega Rápida y Gratuita. Realiza tu pedido hoy y recíbelo en la puerta de tu casa en tiempo récord, sin ningún costo adicional.</p>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-lg-3 mb-4">
+          <div class="feature">
+            <div class="icon">
+              <img src="images/bag.svg" alt="Fácil de Comprar" class="imf-fluid">
+            </div><br>
+            <h3>Fácil de Comprar</h3>
+            <p>Realizar tus compras nunca ha sido tan sencillo. Navega por nuestra tienda online, selecciona tus productos favoritos y completa tu compra.</p>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-lg-3 mb-4">
+          <div class="feature">
+            <div class="icon">
+              <img src="images/support.svg" alt="Soporte 24/7" class="imf-fluid">
+            </div><br>
+            <h3>Soporte 24/7</h3>
+            <p>Con nuestro servicio de Soporte 24/7, puedes contar con asistencia técnica y atención al cliente las 24 horas del día, los 7 días de la semana.</p>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-lg-3 mb-4">
+          <div class="feature">
+            <div class="icon">
+              <img src="images/return.svg" alt="Periodo de devolución" class="imf-fluid">
+            </div><br>
+            <h3>Periodo de devolución</h3>
+            <p>Ofrecemos un período de devolución de X días. Si por alguna razón no estás contento con tu producto, puedes devolverlo sin problemas.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+  <!-- End Why Choose Us Section -->
+  <!-- Start Footer Section -->
+  <footer class="footer-section" style="padding: 1em 0; justify-content: center; align-items: center; position: absolute; bottom: 0; width: 100%;">
+    <div class="container relative">
+      <div class="row g-5 mb-5">
+        <div class="col-lg-4 ">
+          <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">KADA</a></div>
+          <p class="mb-4">Bienvenido a Sistema de información KADA, un sitio web en el cuál podras encontrar buzos únicos y a la moda. En cada prenda aseguramos que obtengas la máxima comodidad y estilo. Nos enorgullece ofrecer una amplia variedad de buzos que se adaptan a todos los gustos y ocasiones. ¡GRACIAS POR ELEGIRNOS!</p>
+        </div>
+        <div class="col-lg-8">
+          <div class="row links-wrap">
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="services.html">Servicios</a></li>
+                <li><a href="contact.html">Contáctanos</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="shop.html#halfzip">Half Zip Sweaters</a></li>
+                <li><a href="shop.html#oversize">Oversize Hoodies</a></li>
+                <li><a href="shop.html#hoodies">Hoodies</a></li>
+                <li><a href="shop.html#crewnecks">Crewnecks</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="LoginAdmi.html">¿Eres Administrador?</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer Section -->
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/tiny-slider.js"></script>
+  <script src="js/custom.js"></script>
+</body>
 </html>
+
